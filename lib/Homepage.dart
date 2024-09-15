96a686bd573bc8e33b76c7637fef4d09299013be
CodeWithIsmail <hmismail360@gmail.com>
2024-09-15 20:26:17 +0600

    Image2Text done

---
+++ lib/Homepage.dart
@@ -0,0 +1,193 @@
+import 'ImportAll.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  File? selectedMedia;
+  final ImagePicker _picker = ImagePicker();
+  String extractedText = "";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text("Text Recognition"),
+      ),
+      body: _buildUI(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          _showPicker(context);
+        },
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+
+  void _showPicker(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      builder: (BuildContext bc) {
+        return SafeArea(
+          child: Wrap(
+            children: <Widget>[
+              ListTile(
+                leading: const Icon(Icons.photo_library),
+                title: const Text('Pick from Gallery'),
+                onTap: () {
+                  _getImage(ImageSource.gallery);
+                  Navigator.of(context).pop();
+                },
+              ),
+              ListTile(
+                leading: const Icon(Icons.photo_camera),
+                title: const Text('Take a Picture'),
+                onTap: () {
+                  _getImage(ImageSource.camera);
+                  Navigator.of(context).pop();
+                },
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  Future<void> _getImage(ImageSource source) async {
+    final pickedFile = await _picker.pickImage(source: source);
+    if (pickedFile != null) {
+      File? croppedFile = await _cropImage(File(pickedFile.path));
+      if (croppedFile != null) {
+        setState(() {
+          selectedMedia = croppedFile;
+        });
+        final text = await _extractText(croppedFile);
+        setState(() {
+          extractedText = text ?? "";
+        });
+      }
+    }
+  }
+
+  Future<File?> _cropImage(File imageFile) async {
+    CroppedFile? croppedFile = await ImageCropper().cropImage(
+      sourcePath: imageFile.path,
+      uiSettings: [
+        AndroidUiSettings(
+          toolbarTitle: 'Crop Image',
+          toolbarColor: Colors.deepOrange,
+          toolbarWidgetColor: Colors.white,
+          aspectRatioPresets: [
+            CropAspectRatioPreset.original,
+            CropAspectRatioPreset.square,
+            CropAspectRatioPresetCustom(),
+          ],
+        ),
+        IOSUiSettings(
+          title: 'Crop Image',
+          aspectRatioPresets: [
+            CropAspectRatioPreset.original,
+            CropAspectRatioPreset.square,
+            CropAspectRatioPresetCustom(),
+          ],
+        ),
+      ],
+    );
+    if (croppedFile != null) {
+      return File(croppedFile.path);
+    }
+    return null;
+  }
+
+  Widget _buildUI() {
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            _imageView(),
+            const SizedBox(height: 20),
+            _extractTextView(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _imageView() {
+    if (selectedMedia == null) {
+      return const Center(
+        child: Text("Pick an image for text recognition."),
+      );
+    }
+    return Center(
+      child: Image.file(
+        selectedMedia!,
+        width: (MediaQuery.sizeOf(context).width * 2) / 3,
+      ),
+    );
+  }
+
+  Widget _extractTextView() {
+    if (selectedMedia == null) {
+      return const Center(
+        child: Text("No result."),
+      );
+    }
+    return Column(
+      children: [
+        FutureBuilder<String?>(
+          future: _extractText(selectedMedia!),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const CircularProgressIndicator();
+            }
+            return Text(
+              snapshot.data ?? extractedText,
+              style: const TextStyle(fontSize: 20),
+            );
+          },
+        ),
+        const SizedBox(height: 20),
+        ElevatedButton(
+          onPressed: () {
+            Clipboard.setData(ClipboardData(text: extractedText));
+            ScaffoldMessenger.of(context).showSnackBar(
+              const SnackBar(content: Text('Text copied to clipboard!')),
+            );
+          },
+          child: const Text('Copy'),
+        ),
+      ],
+    );
+  }
+
+  Future<String?> _extractText(File file) async {
+    final textRecognizer = TextRecognizer(
+      script: TextRecognitionScript.latin,
+    );
+    final InputImage inputImage = InputImage.fromFile(file);
+    final RecognizedText recognizedText =
+        await textRecognizer.processImage(inputImage);
+    String text = recognizedText.text;
+    textRecognizer.close();
+    return text;
+  }
+}
+
+class CropAspectRatioPresetCustom implements CropAspectRatioPresetData {
+  @override
+  (int, int)? get data => (2, 3);
+
+  @override
+  String get name => '2x3 (customized)';
+}
---
+++ lib/ImportAll.dart
@@ -0,0 +1,9 @@
+export 'dart:async';
+export 'dart:developer' hide Flow;
+export 'dart:io';
+export 'package:image_picker/image_picker.dart';
+export 'package:image_cropper/image_cropper.dart';
+export 'package:flutter/material.dart';
+export 'Homepage.dart';
+export 'package:google_mlkit_text_recognition/google_mlkit_text_recognition.dart';
+export 'package:flutter/services.dart';
